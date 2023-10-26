9fe5ced6eb258adf67cf1d7140ffda1551a24e7b
Antony <148582393+fightingantony@users.noreply.github.com>
2023-10-26 22:43:59 +0800

    Update 4f.sql

--- HW1/SQL_query/4f.sql
+++ HW1/SQL_query/4f.sql
@@ -1,15 +1,14 @@
-with theme_color_count as (
-	select themes.name name_of_theme, colors.name name_of_color, sum(inventory_parts.quantity) color_count
-	from themes, colors, sets, inventories, inventory_parts
-	where themes.id = sets.theme_id and colors.id = inventory_parts.color_id
-		  and sets.set_num = inventories.set_num and inventories.id = inventory_parts.inventory_id
-	group by themes.name, colors.name
-),
-max_color_count as(
-	select name_of_theme, name_of_color, color_count, 
-		   rank() over (partition by name_of_theme order by color_count desc) as color_rank
-	from theme_color_count
+with colorrank as (
+    select themes.name theme_name, colors.name most_used_color,
+	       rank() over (partition by themes.id order by sum(inventory_parts.quantity) desc)  color_rank
+    from themes
+    join sets on themes.id = sets.theme_id
+    join inventories on sets.set_num = inventories.set_num
+    join inventory_parts on inventories.id = inventory_parts.inventory_id
+    join colors on inventory_parts.color_id = colors.id
+    group by themes.id, colors.name
 )
-select name_of_theme, name_of_color most_used_color
-from max_color_count
+select theme_name, most_used_color
+from colorrank
 where color_rank = 1
+order by theme_name asc;
