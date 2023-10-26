e55d089cee6b6e690e74e350655eea6206aee06c
Antony <148582393+fightingantony@users.noreply.github.com>
2023-10-26 19:07:53 +0800

    Update 4d.sql

--- HW1/SQL_query/4d.sql
+++ HW1/SQL_query/4d.sql
@@ -1,5 +1,5 @@
 select themes.name name_of_theme, avg(sets.num_parts) avg_num_parts_of_set 
 from themes, sets
 where themes.id = sets.theme_id
-group by themes.name
+group by themes.id
 order by avg_num_parts_of_set asc
